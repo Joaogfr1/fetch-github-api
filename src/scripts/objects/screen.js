7bd52e47686bafd92be138a4920d488e83877fcd
João Gabriel <joaogfr100@gmail.com>
2024-11-13 20:03:10 -0300

    Solicitações resolvidas

--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -8,6 +8,11 @@ const screen = {
                 <div class="data">
                     <h1>${user.name ?? 'Não possui nome cadastrado 😓'}</h1>
                     <p>${user.bio ?? 'Não possui bio cadastrada 😓'}</p>
+
+                    <br>
+
+                    <p class="number"> 👥Número de seguidores: ${user.followers} </p>
+                    <p class="number"> 👥Número de pessoas seguidas: ${user.following} </p>
                 </div>
         </div>
         `
@@ -16,7 +21,15 @@ const screen = {
         user.repositories.forEach(repo => repositoriesItens +=
             `
             <li> 
-                <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+                <a href="${repo.html_url}" target="_blank">${repo.name}
+                    <br><br>
+                    <div class="repo-icons">
+                        <span>🍴${repo.forks}</span> 
+                        <span>🌟${repo.stargazers_count}</span> 
+                        <span>👀${repo.watchers}</span> 
+                        <span>👩‍💻${repo.language}</span>
+                    </div>
+                </a>
             </li>
             `
         )
@@ -30,6 +43,38 @@ const screen = {
             </div>
             `
         }
+
+        let eventsItems = '';
+        user.events.forEach(events => {
+            if (events.type === 'PushEvent' && events.payload.commits.length > 0) {
+                const commitMessage = events.payload.commits[0]?.message || 'Sem mensagem de commit'
+
+                eventsItems += 
+                `
+                <li>
+                    <div class="event-title">${events.repo.name} -</div> ${commitMessage}
+                </li>
+                `
+            } else if (events.type === 'CreateEvent') {
+                eventsItems += 
+                `
+                <li>
+                    <div class="event-title">${events.repo.name} -</div> Sem mensagem de commit 
+                </li>
+                `
+            }
+        });
+    
+        if (user.events.length > 0) {
+            this.userProfile.innerHTML += 
+            `
+            <div class="events">
+                <h2>Eventos</h2>
+                <ul>${eventsItems}</ul>
+            </div>
+            `;
+        }
+
     },
     renderNotFound(){
         this.userProfile.innerHTML = "<h3>Usuário não encontrado</h3>"
